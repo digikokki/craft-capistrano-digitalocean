b22d73f6182eaa10a12ac2c9c4192684ac08fc1b
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2017-08-26 23:06:36 +0300

    apache user and group fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "3.9.0"
 
 set :file_permissions_roles, :all
 set :file_permissions_paths, ["craft/config"]
-set :file_permissions_users, ["root"]
-set :file_permissions_groups, ["root"]
+set :file_permissions_users, ["www-data"]
+set :file_permissions_groups, ["www-data"]
 set :file_permissions_chmod_mode, "0744"
 
 set :application, "craft-capistrano-digitalocean"
